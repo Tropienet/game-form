b723e226ffbefe07a06fde48d772837c3cf54fbc
Tropie <tingoricki1@gmail.com>
2022-03-25 14:59:14 +0100

    Finished form for games and added some CSS for game form

--- src/GameInfoForm.js
+++ src/GameInfoForm.js
@@ -1,10 +1,17 @@
 import React, { useState, useEffect } from "react";
 import firebase from "./firebase";
 import uniqid from "uniqid";
+import Navbar from "./NavBar";
+
+let db = firebase.firestore();
 
 const GameInfoForm = () => {
 
     const [genres, setGenres] = useState([]);
+    const [gameGenre, setGameGenre] = useState("");
+    const [gameName, setGameName] = useState("");
+    const [gameDeveloper, setGameDeveloper] = useState("");
+    const [gameDescription, setGameDescription] = useState("");
 
     const ref = firebase.firestore().collection("genre");
     
@@ -25,20 +32,52 @@ const GameInfoForm = () => {
 
     });
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        db.collection(`${gameGenre}`).add({
+            id: uniqid(),
+            name: gameName,
+            developer: gameDeveloper,
+        })
+        .then(() => {
+            alert("Game has been added");
+        })
+        .catch((error) => {
+            alert(error.message);
+        })
+
+        setGameDeveloper("");
+        setGameName("");
+        setGameGenre("");
+    }
+
     return (
-        <div>
-            <form>
+        <div className="game-form-page-container">
+            <Navbar />
+
+            <form onSubmit={handleSubmit} className="game-info-form">
                 <label>
                     Game Name:
-                    <input type="text" placeholder="Diable 3, Heartstone, League of legends..."></input>
+                    <br></br>
+                    <input  type="text" 
+                            placeholder="Diablo 3, Heartstone, League of legends..."
+                            value={gameName}
+                            onChange={(e) => setGameName(e.target.value)}></input>
                 </label>
                 <label>
                     Developed by:
-                    <input type="text" placeholder="Blizzard, Riot, Valve..."></input>
+                    <br></br>
+                    <input  type="text" 
+                            placeholder="Blizzard, Riot, Valve..."
+                            value={gameDeveloper}
+                            onChange={(e) => setGameDeveloper(e.target.value)}></input>
                 </label>
                 <label>
                     Genre:
-                    <select>
+                    <br></br>
+                    <select value={gameGenre}
+                            onChange={(e) => setGameGenre(e.target.value)}>
                         <option value="">Select a genre</option>
                         {genres.map((genre) => (
                             <option key={genre.id}
@@ -47,6 +86,15 @@ const GameInfoForm = () => {
                                     </option>
                         ))}
                     </select>
+                </label>
+                <label>
+                    Game Description:
+                    <br></br>
+                    <textarea   className="game-description"
+                                placeholder="Describe the game"
+                                value={gameDescription}
+                                onChange={(e) => setGameDescription(e.target.value)}>
+                    </textarea>
                 </label>
                 <button type="submit">Add game</button>
             </form>
