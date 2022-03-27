fff9a7e8155a0bfcaa631afe18714f2bb83f846a
Tropie <tingoricki1@gmail.com>
2022-03-27 21:43:50 +0200

    Added jspdf and download for pdf info about the game

--- src/Game.js
+++ src/Game.js
@@ -1,5 +1,6 @@
 import React from "react";
 import firebase from "./firebase";
+import { jsPDF } from "jspdf";
 
 function Game(props) {
 
@@ -16,12 +17,23 @@ function Game(props) {
         alert("Game has been deleted");
     }
 
+    function printGameInfo() {
+        const doc = new jsPDF();
+
+        doc.text(`Game Name ${gameInfo.name}
+                \nGame Developer ${gameInfo.developer}
+                \nGame Description ${gameInfo.description}`, 10, 10);
+
+        doc.save(`${gameInfo.name}.pdf`);
+    }
+
     return (
         <>
             <p className="game-name">Game Name: {gameInfo.name}</p>
             <p className="game-developer">Game Developer: {gameInfo.developer}</p>
             <p className="game-desc">Game Description: {gameInfo.description}</p>
             <button onClick={() => deleteGame(gameInfo.id)} className="delete-game-btn">Delete game</button>
+            <button onClick={() => printGameInfo()} className="btn">Download pdf about game</button>
         </>
     )
 }
